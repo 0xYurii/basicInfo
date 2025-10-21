81b390fb03e6b552d8cdae9c1d63f6e65d809a23
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-10-21 16:43:32 +0100

    init the express js folder

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.get('/about',(req,res)=>{
 app.get('/contact-me',(req,res)=>{
   res.sendFile(path.join(__dirname, 'contact-me.html'));
 })
+app.use((req,res)=>{
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
+}) 
 
 app.listen(PORT,()=>{
     console.log(`Server running at http://localhost:${PORT}`)
